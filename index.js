3272075f37ba942ee1bf5012419569a00fe64fb1
chsergey80 <chsergey@yandex.ru>
2023-03-03 14:18:38 +0300

    sprint_5 14 commit

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 // базовый файл JS
 import './pages/index.css';
+
+const popups = document.querySelectorAll('.popup');  
+const profileOpenBotton = document.querySelector('.profile__edit-botton');
+const placeOpenButton = document.querySelector('.profile__add-botton'); //
+const buttonsClose = document.querySelectorAll('.popup__close'); 
+
+const initialCards = [
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+];
+
