aaa99c38a9bf1f5a314550fab18b9cb7cb78765d
chsergey80 <chsergey@yandex.ru>
2023-03-05 03:46:46 +0300

    sprint_5 18 commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+/* конфигурация babel */
+const presets = [
+  ['@babel/preset-env', {
+    targets: {
+      edge: '17',
+      ie: '11',
+      firefox: '50',
+      chrome: '64',
+      safari: '11.1'
+    },
+    useBuiltIns: "entry"
+  }]
+];
+
+module.exports = { presets };
